1d661c905fe14be81c0aac458619b49a4446c6df
AaronFeng753 <52553688+AaronFeng753@users.noreply.github.com>
2019-08-23 14:53:20 +0800

    v1.3

--- Waifu2x-Extension.py
+++ Waifu2x-Extension.py
@@ -1766,6 +1766,7 @@ def checkUpdate(Version_current):
 		input('press any key to return')
 		
 def checkUpdate_start(Version_current):
+	print('Checking update....')
 	headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 	r1=requests.get('https://github.com/AaronFeng753/Waifu2x-Extension/releases/latest',headers=headers)
 	
